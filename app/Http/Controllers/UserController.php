ed85a9f752dc2132befca9820156029c83d4da88
David Vid <davidyusufrahmanto@gmail.com>
2024-04-22 17:33:38 +0700

    minor changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\DataTables\UsersDataTable;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(UsersDataTable $dataTable)
     {
         $users = User::paginate();
 
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::redirect('/', '/login');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [
+    App\Http\Controllers\HomeController::class,
+    'index',
+])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::view('about', 'about')->name('about');
 
-    Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+    Route::get('users', [
+        \App\Http\Controllers\UserController::class,
+        'index',
+    ])->name('users.index');
 
-    Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
-    Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+    Route::get('profile', [
+        \App\Http\Controllers\ProfileController::class,
+        'show',
+    ])->name('profile.show');
+    Route::put('profile', [
+        \App\Http\Controllers\ProfileController::class,
+        'update',
+    ])->name('profile.update');
 });
