f9480528534980ca4dd78e3be86b8d41ffab289d
David Vid <davidyusufrahmanto@gmail.com>
2024-04-24 10:57:35 +0700

    add package:datatables.net

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import './bootstrap'
-import pdfmake from 'pdfmake'
+import 'jquery'
+import pdfMake from 'pdfmake/build/pdfmake'
+import pdfFonts from 'pdfmake/build/vfs_fonts'
 import DataTable from 'datatables.net-bs5'
 import 'datatables.net-buttons-bs5'
 import 'datatables.net-buttons/js/buttons.html5.mjs'
@@ -7,7 +9,26 @@ import 'datatables.net-buttons/js/buttons.print.mjs'
 import 'datatables.net-staterestore-bs5'
 import 'datatables.net-responsive-bs5'
 
+pdfMake.vfs = pdfFonts.pdfMake.vfs
+
 new DataTable('#dataTables', {
-    dom: 'Bfrtip',
     responsive: true,
+    dom: 'Bfrtip',
+    layout: {
+        topStart: {
+            buttons: [
+                {
+                    extend: 'pdf',
+                    text: 'PDF',
+                    customize: function (doc) {
+                        doc.content.splice(1, 0, {
+                            margin: [0, 0, 0, 12],
+                            alignment: 'center',
+                        })
+                    },
+                },
+                {},
+            ],
+        },
+    },
 })
