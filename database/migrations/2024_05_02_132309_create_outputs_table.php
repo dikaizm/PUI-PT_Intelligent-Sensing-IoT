11519dd5125a3a83e57454992c17e8073c8a6e9e
David Vid <davidyusufrahmanto@gmail.com>
2024-05-02 15:15:07 +0700

    add migrations:output

--- database/migrations/2024_05_02_132309_create_outputs_table.php
+++ database/migrations/2024_05_02_132309_create_outputs_table.php
@@ -4,15 +4,61 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('outputs', function (Blueprint $table) {
+        Schema::create('output', function (Blueprint $table) {
             $table->id();
+            $table->smallInteger('jenis_dokumen_id')->unsigned();
+            $table
+                ->foreign('jenis_dokumen_id')
+                ->references('id')
+                ->on('jenis_dokumen')
+                ->onDelete('restrict');
+            $table->foreignId('penelitian_id');
+            $table
+                ->foreign('penelitian_id')
+                ->references('id')
+                ->on('penelitian')
+                ->onDelete('restrict');
+            $table->foreignId('publisher_id');
+            $table
+                ->foreign('publisher_id')
+                ->references('id')
+                ->on('publisher')
+                ->onDelete('restrict');
+            $table->smallInteger('status_jurnal_id')->unsigned();
+            $table
+                ->foreign('status_jurnal_id')
+                ->references('id')
+                ->on('status_jurnal')
+                ->onDelete('restrict');
+            $table->text('keterangan');
+            $table->string('tautan');
+            $table->timestamps();
+        });
+        Schema::create('publisher_key', function (Blueprint $table) {
+            $table->smallInteger('id')->unsigned()->autoIncrement()->primary();
+            $table->string('name', 64);
+            $table->timestamp();
+        });
+        Schema::create('publisher', function (Blueprint $table) {
+            $table->id();
+            $table->smallInteger('publisher_key_id')->unsigned();
+            $table
+                ->foreign('publisher_key_id')
+                ->references('id')
+                ->on('publisher_key')
+                ->onDelete('restrict');
+            $table->string('tingkat_index', 32);
+            $table->timestamp();
+        });
+        Schema::create('status_jurnal', function (Blueprint $table) {
+            $table->smallInteger('id')->unsigned()->autoIncrement()->primary();
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -22,6 +68,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('outputs');
+        Schema::dropIfExists('output');
+        Schema::dropIfExists('publisher_key');
+        Schema::dropIfExists('publisher');
+        Schema::dropIfExists('status_jurnal');
     }
 };
